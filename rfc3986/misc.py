7d94cea471bf7cab8248d6d6e0822ac21a182f38
Ian Cordasco <ian.cordasco@rackspace.com>
2014-06-24 09:28:23 -0500

    Regular expressions

--- rfc3986/misc.py
+++ rfc3986/misc.py
@@ -7,3 +7,16 @@ GENERIC_DELIMITERS = (":", "/", "?", "#", "[", "]", "@")
 # Delimiters of subcomponents
 SUB_DELIMITERS = ("!", "$", "&", "'", "(", ")", "*", "+", ",", ";", "=")
 RESERVED_CHARS = general_delimiters + sub_delimiters
+
+
+scheme_pattern = '\w[\w\d+-.]*'
+authority_pattern = '[^/?#]+'
+path_pattern = '.*'  # FIXME
+userinfo_pattern = ''
+
+expression = ('(?P<scheme>{scheme}):(//(?P<authority>{authority})|'
+              '(?P<path>{path}))').format({
+                  'scheme': scheme_pattern,
+                  'authority': authority_pattern,
+                  'path': path_pattern,
+                  })
