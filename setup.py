2c4e606b350b38e561c94f6f3eafbaa720081266
Ian Cordasco <graffatcolmingov@gmail.com>
2017-03-04 14:14:29 -0600

    Start working towards 1.0

--- setup.py
+++ setup.py
@@ -1,19 +1,13 @@
-#!/usr/bin/env python
-
+"""Packaging logic for the rfc3986 library."""
 import io
 import os
 import sys
 
+import setuptools
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))  # noqa
+
 import rfc3986
-
-try:
-    from setuptools import setup
-except ImportError:
-    from distutils.core import setup
-
-if sys.argv[-1] == 'publish':
-    os.system('python setup.py bdist_wheel sdist upload')
-    sys.exit()
 
 packages = [
     'rfc3986',
@@ -25,15 +19,16 @@ with io.open('README.rst', encoding='utf-8') as f:
 with io.open('HISTORY.rst', encoding='utf-8') as f:
     history = f.read()
 
-setup(
+setuptools.setup(
     name='rfc3986',
     version=rfc3986.__version__,
     description='Validating URI References per RFC 3986',
     long_description=readme + '\n\n' + history,
     author='Ian Cordasco',
-    author_email='ian.cordasco@rackspace.com',
-    url='https://rfc3986.readthedocs.org',
+    author_email='graffatcolmingov@gmail.com',
+    url='http://rfc3986.readthedocs.io',
     packages=packages,
+    package_dir={'': 'src/'},
     package_data={'': ['LICENSE']},
     include_package_data=True,
     license='Apache 2.0',
@@ -43,10 +38,11 @@ setup(
         'Natural Language :: English',
         'License :: OSI Approved :: Apache Software License',
         'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ),
 )
