283f910fff531f0cc386767de1372c690929372b
Sachaa-Thanasius <111999343+Sachaa-Thanasius@users.noreply.github.com>
2024-07-03 08:44:50 -0400

    Add exclude lines to .coveragerc to account for a) `if t.TYPE_CHECKING` blocks and b) lines that are only ellipses, as well as some temporary pragma comments in _typing_compat.py. This seems to account for most of the missing coverage with the current configuration, excluding line 447 in validators.py.

--- src/rfc3986/_typing_compat.py
+++ src/rfc3986/_typing_compat.py
@@ -3,11 +3,11 @@ import typing as t
 
 __all__ = ("Self",)
 
-if sys.version_info >= (3, 11):
+if sys.version_info >= (3, 11):  # pragma: no cover
     from typing import Self
 elif t.TYPE_CHECKING:
     from typing_extensions import Self
-else:
+else:  # pragma: no cover
 
     class _PlaceholderMeta(type):
         # This is meant to make it easier to debug the presence of placeholder
